426f00766830164fd60a9d9aebc7f14f4825e695
Zach Baldwin <electronicstinkerer@gmail.com>
2022-11-06 12:38:22 -0500

    Create function signatures for sequencer

---
+++ sequencer/software/module.h
@@ -0,0 +1,44 @@
+/**
+ * MODULE CONTROLLER
+ * 
+ * Authors:
+ * Zach Baldwin
+ * Andy Regli
+ * Mark Vaughn
+ *
+ */
+
+#ifndef DRUM_MODULE_H
+#define DRUM_MODULE_H
+
+// Useful things
+#define True 1
+#define False 0
+
+// Tempo settings
+#define BPM_MIN 20
+#define BPM_DEFAULT 120
+#define BPM_MAX 600
+
+// Time signature settings
+#define TS_DEFAULT = 1;
+
+unsigned int time_signatures[][2] = {
+    {3, 4},
+    {4, 4},
+    {7, 8}
+};
+
+
+// Function signatures
+void update_screen(void);
+void update_buttons(void);
+void isr_timer(void);
+void isr_module_status(void);
+void isr_tempo_encoder(void);
+void isr_play_pause(void);
+void isr_time_sig_encoder(void);
+void isr_pad_event(void); // This might not be possible, depending on whether or not the pad can send interrupts
+
+#endif
+
