4352ec0e879503c20be35052be088b5c5d0c4d5d
Zach Baldwin <electronicstinkerer@gmail.com>
2022-11-09 15:06:41 -0500

    Add GPIO interrupt setup

--- sequencer/software/sequencer.c
+++ sequencer/software/sequencer.c
@@ -8,6 +8,8 @@
  *
  */
 
+#include "hardware/gpio.h"
+
 #include "serbus.h"
 #include "sequencer.h"
 
@@ -17,17 +19,76 @@ volatile unsigned int is_mod_selected;
 volatile unsigned int bpm;
 volatile unsigned int time_sig;
 volatile unsigned int current_beat;
+repeating_timer_t timer;
 
 int main ()
 {
-    // Initialize everything
-    is_running = False;
-    is_mod_selected = False;
+    // Hardware init
+    stdio_init_all();
+
+    // Initialize Sequencer states
+    is_running = false;
+    is_mod_selected = false;
     bpm = BPM_DEFAULT;
     time_sig = TS_DEFAULT;
     current_beat = 0;
 
-    // TODO: Register ISRs to GPIO and Timer events
+    // Register ISRs to GPIO and Timer events
+    // Tempo Encoder
+    gpio_set_irq_enabled_with_callback(
+        TEMPO_ENC0,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true, // Enable interrupt
+        &isr_tempo_encoder
+        );
+    gpio_set_irq_enabled_with_callback(
+        TEMPO_ENC1,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true, // Enable interrupt
+        &isr_tempo_encoder
+        );
+
+    // Time signature encoder
+    gpio_set_irq_enabled_with_callback(
+        GPIO_ENC0,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true, // Enable interrupt
+        &isr_time_sig_encoder
+        );
+    gpio_set_irq_enabled_with_callback(
+        TSIG_ENC1,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true, // Enable interrupt
+        &isr_time_sig_encoder
+        );
+
+    // Play / pause button
+    gpio_set_irq_enabled_with_callback(
+        PLYSTP_BTN,
+        GPIO_IRQ_EDGE_FALL,
+        true, // Enable interrupt
+        &isr_play_pause
+        );
+
+    // Module selection status update
+    gpio_set_irq_enabled_with_callback(
+        MOD_STAT_IRQ,
+        GPIO_IRQ_EDGE_RISE,
+        true, // Enable interrupt
+        &isr_module_status
+        );
+
+    // Set up the tick timer for micro beat generation    
+
+    // Negative timeout means exact delay (rather than delay between callbacks)
+    if (!add_repeating_timer_us((-1000000 * 60) / bpm, timer_callback, NULL, &isr_timer)) {
+        // printf("Failed to add timer\n");
+        // ERROR!
+        return 1;
+    }
+
+
+
     
 
     // Main event loop
@@ -44,7 +105,7 @@ int main ()
  */
 void update_screen(void)
 {
-    
+    // TODO
 }
 
 
@@ -54,54 +115,67 @@ void update_screen(void)
  */
 void update_buttons(void)
 {
-
+    // TODO
 }
 
 
 /**
  * Handles generation of synchronization "tick" pulses with all modules
  */
-void isr_timer(void)
+bool isr_timer(repeating_timer_t *rt);
 {
+    // TODO!
 
+    return true; // keep repeating    
 }
 
 
 /**
  * Handles detection of module status changes via the CBUS_EXTRA 
  * interrupt line
- */
-void isr_module_status(void)
+ * 
+ * @param The GPIO pin number
+ * @param The GPIO event type */
+void isr_module_status(unsigned int gpio, uint32_t event)
 {
-
+    // TODO
 }
 
 
 /**
  * Handles updating of the global tempo value when the tempo encoder
  * is rotated
+ * 
+ * @param The GPIO pin number
+ * @param The GPIO event type
  */
-void isr_tempo_encoder(void)
+void isr_tempo_encoder(unsigned int gpio, uint32_t event)
 {
-
+    // TODO
 }
 
 
 /**
  * Handles the toggling of the is_running flag based on the play/pause
  * button input
+ * 
+ * @param The GPIO pin number
+ * @param The GPIO event type
  */
-void isr_play_pause(void)
+void isr_play_pause(unsigned int gpio, uint32_t event)
 {
-
+    is_running = !is_running;
 }
 
 
 /**
  * Handles cycling of the time signature when the time signature 
  * encoder is rotated
+ * 
+ * @param The GPIO pin number
+ * @param The GPIO event type
  */
-void isr_time_sig_encoder(void)
+void isr_time_sig_encoder(unsigned int gpio, uint32_t event)
 {
 
 }
@@ -110,10 +184,13 @@ void isr_time_sig_encoder(void)
 // TODO: This might not be possible, depending on whether or not the pad can s
 /**
  * Handle a button press on the squish pad
+ * 
+ * @param The GPIO pin number
+ * @param The GPIO event type
  */
 void isr_pad_event(void)
 {
-
+    // TODO
 }
 
 
--- sequencer/software/sequencer.h
+++ sequencer/software/sequencer.h
@@ -11,9 +11,47 @@
 #ifndef DRUM_SEQUENCER_H
 #define DRUM_SEQUENCER_H
 
-// Useful things
-#define True 1
-#define False 0
+// PINS
+#define GPIO_TP9        0
+#define GPIO_ENC00      1
+#define GPIO_ENC01      2
+#define GPIO_TP10       3
+#define GPIO_I2C0_SDA0  4
+#define GPIO_I2C0_SCL0  5
+#define GPIO_ENC10      6
+#define GPIO_ENC11      7
+// GPIO 8
+// GPIO 9
+#define GPIO_ENC20      10
+#define GPIO_ENC21      11
+#define GPIO_I2C0_SDA1  12
+#define GPIO_I2C0_SCL1  13
+#define GPIO_ENC30      14
+#define GPIO_ENC31      15
+#define GPIO_CBUS_EXTRA 16
+#define GPIO_CBUS_DRDY  17
+#define GPIO_CBUS_SCK   18
+#define GPIO_CBUS_SDIN  19
+#define GPIO_CBUS_SDOUT 20
+// GPIO 21
+#define GPIO_CBUS_NXTPR 22
+#define GPIO_BTN0       23
+#define GPIO_BTN1       24
+// GPIO 25
+#define GPIO_ADC0       26
+#define GPIO_ADC1       27
+#define GPIO_ADC2       28
+#define GPIO_ADC3       29
+
+
+// Named pin mapping
+#define TEMPO_ENC0      GPIO_ENC00
+#define TEMPO_ENC1      GPIO_ENC01
+#define TSIG_ENC0       GPIO_ENC10
+#define TSIG_ENC1       GPIO_ENC11
+#define PLYSTP_BTN      GPIO_BTN0
+#define MOD_STAT_IRQ    GPIO_CBUS_EXTRA
+
 
 // Tempo settings
 #define BPM_MIN 20
@@ -33,11 +71,11 @@ unsigned int time_signatures[][2] = {
 // Function signatures
 void update_screen(void);
 void update_buttons(void);
-void isr_timer(void);
-void isr_module_status(void);
-void isr_tempo_encoder(void);
-void isr_play_pause(void);
-void isr_time_sig_encoder(void);
+bool isr_timer(repeating_timer_t *rt);
+void isr_module_status(unsigned int, uint32_t);
+void isr_tempo_encoder(unsigned int, uint32_t);
+void isr_play_pause(unsigned int, uint32_t);
+void isr_time_sig_encoder(unsigned int, uint32_t);
 void isr_pad_event(void); // This might not be possible, depending on whether or not the pad can send interrupts
 
 #endif
